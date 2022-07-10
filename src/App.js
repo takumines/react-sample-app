73655c8d87ca981f825d98b6251beee1de6d4a4d
takumi_kurogi <takumines1@gmail.com>
2022-07-11 00:52:21 +0900

    pagesコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import "./styles.css";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { BrowserRouter } from "react-router-dom";
-import {DefaultLayout} from "./components/templates/DefaultLayout";
-
-const user = {
-  image: "https://source.unsplash.com/Mv9hjnEUHR4",
-  name: "太郎",
-  email: "test@example.com",
-  phone: "090-1111-2222",
-  company: {
-    name: "テスト会社",
-  },
-  website: "https://google.com"
-}
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br/>
-        <SearchInput/>
-        <UserCard user={user}/>
-      </DefaultLayout>
-    </BrowserRouter>
+    <Router></Router>
   );
 };
