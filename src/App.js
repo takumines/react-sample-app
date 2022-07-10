8691bc084d9f3f44d4f7d7afedadcb7179d8a737
takumi_kurogi <takumines1@gmail.com>
2022-07-11 00:19:51 +0900

    organismsコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import "./styles.css";
 import {PrimaryButton} from "./components/atoms/button/PrimaryButton";
 import {SecondaryButton} from "./components/atoms/button/SecondaryButton";
 import {SearchInput} from "./components/molecules/SearchInput";
+import {UserCard} from "./components/organisms/user/UserCard";
+
+const user = {
+  image: "https://source.unsplash.com/Mv9hjnEUHR4",
+  name: "太郎",
+  email: "test@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト会社",
+  },
+  website: "https://google.com"
+}
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br/>
       <SearchInput/>
+      <UserCard user={user}/>
     </div>
   );
 };
