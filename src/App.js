6f295e6da33507de2682b2975063cb2f156fe50b
takumi_kurogi <takumines1@gmail.com>
2022-07-10 22:21:38 +0900

    ルーティング処理の切り出し

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import "./styles.css";
 import { useState, useCallback } from "react";
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Link } from "react-router-dom"
 import { ChildArea } from "./ChildArea";
 import { InlineStyle } from "./components/InlineStyle";
 import { CssModules } from "./components/CssModules";
 import { StyledJsx } from "./components/StyledJsx";
 import { StyledComponents } from "./components/StyledComponents"
 import { Emotion } from "./components/Emotion";
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
+import { Router } from "./router/Router";
 
 export default function App() {
   const [text, setText] = useState("");
@@ -41,27 +37,7 @@ export default function App() {
         <StyledComponents />
         <Emotion />
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route path="/page1" render={({ match: { url } }) => (
-          <Switch>
-            <Route exact path={url}>
-              <Page1/>
-            </Route>
-            <Route exact path={`${url}/detailA`}>
-              <Page1DetailA/>
-            </Route>
-            <Route exact path={`${url}/detailB`}>
-              <Page1DetailB/>
-            </Route>
-          </Switch>
-        )} />
-        <Route path="/page2">
-          <Page2/>
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
