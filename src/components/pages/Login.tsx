ff0e85109d6a704e3ca506f6ddae18cfbcff2e33
takumi_kurogi <takumines1@gmail.com>
2022-08-08 23:23:06 +0900

    ログイン機能作成

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,31 @@
-import { FC, memo } from 'react'
+import { ChangeEvent, FC, memo, useState } from 'react'
+import { Box, Divider, Flex, Heading, Input, Stack } from '@chakra-ui/react'
 
-const Login: FC = () => <p>ログインページです</p>
+import PrimaryButton from '../atoms/button/PrimaryButton'
+import { useAuth } from '../../hooks/useAuth'
+
+const Login: FC = () => {
+  const [userId, setUserId] = useState('')
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => setUserId(e.target.value)
+  const { login, loading } = useAuth()
+  const onClickLogin = () => login(userId)
+
+  return (
+    <Flex align='center' justify='center' height='100vh'>
+      <Box bg='white' w='sm' p={4} borderRadius='md' shadow='md'>
+        <Heading as='h2' size='lg' textAlign='center'>
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder='ユーザーID' value={userId} onChange={onChangeUserId} />
+          <PrimaryButton disabled={userId === ''} loading={loading} onClick={onClickLogin}>
+            ログイン
+          </PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  )
+}
 
 export default memo(Login)
---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,34 @@
+import { useCallback, useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+import { User } from '../types/api/user'
+
+export const useAuth = () => {
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false)
+
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true)
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            navigate('/home')
+          } else {
+            // eslint-disable-next-line no-alert
+            alert('ユーザーが見つかりません。')
+          }
+        })
+        .catch(() => {
+          // eslint-disable-next-line no-alert
+          alert('ログインできません')
+        })
+        .finally(() => setLoading(false))
+    },
+    [navigate]
+  )
+
+  return { login, loading } as const
+}
