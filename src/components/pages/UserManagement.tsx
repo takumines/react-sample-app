62dc098db77ddc8afb2d5ed0870cdc2438dd834b
takumi_kurogi <takumines1@gmail.com>
2022-08-09 21:19:33 +0900

    ユーザーを表示するカードコンポーネント作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,13 @@
 import { FC, memo } from 'react'
+import { Wrap, WrapItem } from '@chakra-ui/react'
+import UserCard from '../organisms/user/UserCard'
 
-const UserManagement: FC = () => <p>ユーザー管理ページです</p>
+const UserManagement: FC = () => (
+  <Wrap p={{ base: 4, md: 10 }}>
+    <WrapItem>
+      <UserCard imageUrl='https://source.unsplash.com/random' userName='tetes' fullName='fdfdfsd' />
+    </WrapItem>
+  </Wrap>
+)
 
 export default memo(UserManagement)
