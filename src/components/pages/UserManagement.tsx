42dd1e65815d5fb2b9451c3ad6accd4ec81129fe
takumi_kurogi <takumines1@gmail.com>
2022-08-12 22:56:08 +0900

    ユーザー一覧取得処理の作成aaa

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,33 @@
-import { FC, memo } from 'react'
-import { Wrap, WrapItem } from '@chakra-ui/react'
+import { FC, memo, useEffect } from 'react'
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react'
 import UserCard from '../organisms/user/UserCard'
+import { useAllUsers } from '../../hooks/useAllUsers'
 
-const UserManagement: FC = () => (
-  <Wrap p={{ base: 4, md: 10 }}>
-    <WrapItem>
-      <UserCard imageUrl='https://source.unsplash.com/random' userName='tetes' fullName='fdfdfsd' />
-    </WrapItem>
-  </Wrap>
-)
+const UserManagement: FC = () => {
+  const { getUsers, loading, users } = useAllUsers()
+  useEffect(() => getUsers(), [])
+
+  return (
+    <>
+      {loading ? (
+        <Center h='100vh'>
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users?.map((user) => (
+            <WrapItem key={user.id} mx='auto'>
+              <UserCard
+                imageUrl='https://source.unsplash.com/random'
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  )
+}
 
 export default memo(UserManagement)
